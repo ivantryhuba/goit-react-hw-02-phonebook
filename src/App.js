4ffa78fb463fec4200899dc6a40736d9511beb17
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-07-17 23:46:57 +0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+class App extends React.Component {}
+export default App;
---
+++ src/components/Input/Input.js
@@ -0,0 +1,6 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Input = ({ type, name, pattern, title, required }) => {
+  
+};
